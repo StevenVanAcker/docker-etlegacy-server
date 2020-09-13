3a89d745fa72bf827f4e8e47b35120f7a2fc6d36
Steven Van Acker <steven@singularity.be>
2020-09-14 01:39:34 +0200

    maps

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 cn := etlegacy-server
-runargs := --rm -ti -v $$PWD:/data -p 11180:80 -p 27960:27960/udp --env-file=config
+runargs := --rm -ti -v $$PWD/maps:/maps -p 11180:80 -p 27960:27960/udp --env-file=config
 
 build:
 	docker build $(buildargs) -t $(cn) .
--- init.sh
+++ init.sh
@@ -1,6 +1,23 @@
 #!/bin/sh -e
 
 cd /home/etuser/et
+# process config files
 envsubst < etmain/myconfig.cfg.template > etmain/myconfig.cfg
 envsubst < legacy/omni-bot/et/user/omni-bot.cfg.template > legacy/omni-bot/et/user/omni-bot.cfg
+
+# link to maps
+if [ -d /maps ];
+then
+	for i in /maps/*;
+	do
+		n=$(basename $i)
+		target=/home/etuser/et/etmain/$n
+		if [ ! -e $target ]; then 
+			echo "Creating link to map file $i from $target";
+			ln -s $i $target; 
+		fi
+	done
+fi
+
+# start server
 ./etlded +set g_protect 1 +set omnibot_enable 1 +set omnibot_path "./legacy/omni-bot" +exec etl_server.cfg
